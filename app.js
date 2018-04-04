fe7ad24d91228dd98dace2e42760f87737d83462
Dibakar <crossbonesdipu@gmail.com>
2018-04-04 22:04:05 +0600

    3 Modules of JS APP done

---
+++ app.js
@@ -0,0 +1,32 @@
+var budgetController = (function() {
+
+	var x = 23;
+	var add = function(a) {
+		return x + a;
+	}
+
+	return {
+		publicTest: function(b) {
+			return add(b);
+		}
+	}
+
+})();
+
+var UIController = (function() {
+
+	// Some Codes for later
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl){
+
+	var z = budgetCtrl.publicTest(5);
+
+	return {
+		anotherPublic: function() {
+			console.log(z);
+		}
+	}
+
+})(budgetController, UIController);
