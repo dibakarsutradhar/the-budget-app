85592ec028f669624cb9989d1fbe8b99f2c87869
Dibakar <crossbonesdipu@gmail.com>
2018-04-05 22:40:18 +0600

    Global App Cntrl ToDoList

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
+
+// BUDGET CONTROLLER
 var budgetController = (function() {
 
 	// Some Codes for Later
 
 })();
 
+
+// UI CONTROLLER
 var UIController = (function() {
 
 	// Some Codes for later
 
 })();
 
+
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
+	document.querySelector('.add__btn').addEventListener('click', function() {
+		
+		// 1. Get the field input data
+
+		// 2. Add the item to the budget controller
+
+		// 3. Add the item to the UI
+
+		// 4. Calculate the budget
+
+		// 5. Display the budget on the UI
+	});
 
 })(budgetController, UIController);
