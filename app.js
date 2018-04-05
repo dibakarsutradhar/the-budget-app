b5153ce918900f73d65b2b74d7a67840f1a1a817
Dibakar <crossbonesdipu@gmail.com>
2018-04-05 22:55:17 +0600

    Enter Key Press Added

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
-	document.querySelector('.add__btn').addEventListener('click', function() {
-		
+	var ctrlAddItem = function () {
 		// 1. Get the field input data
 
 		// 2. Add the item to the budget controller
@@ -29,6 +28,16 @@ var controller = (function(budgetCtrl, UICtrl){
 		// 4. Calculate the budget
 
 		// 5. Display the budget on the UI
+
+		console.log('It Works.');
+	}
+
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+	document.addEventListener('keypress', function(event) {
+		if (event.keyCode === 13 || event.which === 13) {
+			ctrlAddItem();
+		}
 	});
 
 })(budgetController, UIController);
