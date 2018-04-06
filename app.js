6c5d42c452e71a80150c2cdfcffd8d4b535f637e
Dibakar <crossbonesdipu@gmail.com>
2018-04-06 23:07:16 +0600

    Added an initialization fn

--- app.js
+++ app.js
@@ -37,12 +37,21 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
-	var DOM = UICtrl.getDOMstrings();
+	var setupEventListeners = function() {
+		var DOM = UICtrl.getDOMstrings();
+		
+		document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+		document.addEventListener('keypress', function(event) {
+			if (event.keyCode === 13 || event.which === 13) {
+				ctrlAddItem();
+			}
+		});
+	};
 
 	var ctrlAddItem = function () {
 		// 1. Get the field input data
 		var input = UICtrl.getInput();
-		console.log(input);
 
 		// 2. Add the item to the budget controller
 
@@ -53,12 +62,14 @@ var controller = (function(budgetCtrl, UICtrl){
 		// 5. Display the budget on the UI
 	}
 
-	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-	document.addEventListener('keypress', function(event) {
-		if (event.keyCode === 13 || event.which === 13) {
-			ctrlAddItem();
+	return {
+		init: function() {
+			console.log('Application has started.');
+			setupEventListeners();
 		}
-	});
+	};
 
 })(budgetController, UIController);
+
+
+controller.init();
