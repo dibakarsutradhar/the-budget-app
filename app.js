85635f158cd68d1810aecb6d107da295aa800bca
Dibakar <crossbonesdipu@gmail.com>
2018-04-07 17:43:24 +0600

    Income HTML Added

--- app.js
+++ app.js
@@ -83,6 +83,19 @@ var UIController = (function() {
 			};	
 		},
 
+		addListItem: function(obj, type) {
+
+			var html;
+
+			// Create HTML string with placeholder text
+			html = '<div class="item clearfix" id="income-0"><div class="item__description">Salary</div><div class="right clearfix"><div class="item__value">+ 2,100.00</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+
+			// Replace placeholder with some actual data
+
+
+			// Insert HTML into the DOM
+		},
+
 		getDOMstrings: function() {
 			return DOMstrings;
 		}
