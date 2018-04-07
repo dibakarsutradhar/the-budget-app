9f1467dc12396205ba0728cb9a71b870ec9df229
Dibakar <crossbonesdipu@gmail.com>
2018-04-07 18:20:04 +0600

    Add Item to the UI

--- app.js
+++ app.js
@@ -71,7 +71,9 @@ var UIController = (function() {
 		inputType: '.add__type',
 		inputDescription: '.add__description',
 		inputValue: '.add__value',
-		inputBtn: '.add__btn'
+		inputBtn: '.add__btn',
+		incomeContainer: '.income__list',
+		expensesContainer: '.expenses__list'
 	};
 
 	return {
@@ -85,20 +87,26 @@ var UIController = (function() {
 
 		addListItem: function(obj, type) {
 
-			var html;
+			var html, newHtml, element;
 
 			// Create HTML string with placeholder text
 			if(type === 'inc') {
+				element = DOMstrings.incomeContainer;
+
 				html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
 			} else if (type === 'exp') {
+				element = DOMstrings.expensesContainer;
+
 				html = '<div class="item clearfix" id="expenses-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
 			}
 			
-			
 			// Replace placeholder with some actual data
-
+			newHtml = html.replace('%id%', obj.id);
+			newHtml = newHtml.replace('%description%', obj.description);
+			newHtml = newHtml.replace('%value%', obj.value);
 
 			// Insert HTML into the DOM
+			document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 		},
 
 		getDOMstrings: function() {
@@ -134,7 +142,8 @@ var controller = (function(budgetCtrl, UICtrl){
 		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
 		// 3. Add the item to the UI
-
+		UICtrl.addListItem(newItem, input.type);
+		
 		// 4. Calculate the budget
 
 		// 5. Display the budget on the UI
