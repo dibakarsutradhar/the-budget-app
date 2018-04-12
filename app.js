373fb6198a3413e1caa801d9ac40531d9a4e4aeb
Dibakar <crossbonesdipu@gmail.com>
2018-04-12 20:51:54 +0600

    Display Budget Method

--- app.js
+++ app.js
@@ -113,7 +113,11 @@ var UIController = (function() {
 		inputValue: '.add__value',
 		inputBtn: '.add__btn',
 		incomeContainer: '.income__list',
-		expensesContainer: '.expenses__list'
+		expensesContainer: '.expenses__list',
+		budgetLabel: '.budget__value',
+		incomeLabel: '.budget__income--value',
+		expensesLabel: '.budget__expenses--value',
+		percentageLabel: '.budget__expenses--percentage'
 	};
 
 	return {
@@ -163,6 +167,13 @@ var UIController = (function() {
 			fieldsArr[0].focus();
 		},
 
+		displayBudget: function(obj) {
+			document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+			document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+			document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+			document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+		},
+
 		getDOMstrings: function() {
 			return DOMstrings;
 		}
@@ -195,7 +206,7 @@ var controller = (function(budgetCtrl, UICtrl){
 		var budget = budgetCtrl.getBudget();
 
 		// 3. Display the budget on the UI
-		console.log(budget);
+		UICtrl.displayBudget(budget);
 	};
 
 	var ctrlAddItem = function () {
