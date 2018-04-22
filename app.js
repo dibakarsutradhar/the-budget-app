25ff41db0bb264c6decc35e2e3cb50f2d9dd36b9
Dibakar <crossbonesdipu@gmail.com>
2018-04-22 23:15:00 +0600

    Display Month Year added

--- app.js
+++ app.js
@@ -160,7 +160,8 @@ var UIController = (function() {
 		expensesLabel: '.budget__expenses--value',
 		percentageLabel: '.budget__expenses--percentage',
 		container: '.container',
-		expensesPercLevel: '.item__percentage'
+		expensesPercLevel: '.item__percentage',
+		dateLabel: '.budget__title--month'
 	};
 
 	var formatNumber = function(num, type) {
@@ -279,6 +280,17 @@ var UIController = (function() {
 			});
 		},
 
+		displayMonth: function() {
+			var now, months, month, year;
+
+			now = new Date();
+			months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+			month = now.getMonth();
+			year = now.getFullYear();
+
+			document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+		},
+
 		getDOMstrings: function() {
 			return DOMstrings;
 		}
@@ -382,6 +394,7 @@ var controller = (function(budgetCtrl, UICtrl){
 	return {
 		init: function() {
 			console.log('Application has started.');
+			UICtrl.displayMonth();
 			UICtrl.displayBudget({
 				budget: 0,
 				totalInc: 0,
