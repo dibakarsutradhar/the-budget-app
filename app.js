35675aecafd6cc20362683f3d4f49de915e0eb01
Dibakar <crossbonesdipu@gmail.com>
2018-04-22 23:01:04 +0600

    Display Budget Updated with formatNumber

--- app.js
+++ app.js
@@ -244,10 +244,12 @@ var UIController = (function() {
 		},
 
 		displayBudget: function(obj) {
-			document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-			document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
-			document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
-			document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+			var type;
+			obj.budget > 0 ? type = 'inc' : type = 'exp';
+
+			document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type);
+			document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, 'inc');
+			document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(obj.totalExp, 'exp');
 
 			if (obj.budget > 0) {
 				document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage +'%';
